ee6918b2523856f7e8d1a6b6843b027c42d9f73e
shelliees <xiaoransun@yahoo.com>
2019-10-07 19:06:53 -0700

    Zimino schema, and zimino website without the serlvet files.
    The front page, menu page is complete, and all the other pages only needs the database part. I'm also uploading the Zimino schema, and the view and insert sql file.

---
+++ Zimino schema/zimino_menu.sql
@@ -0,0 +1,56 @@
+-- MySQL dump 10.13  Distrib 8.0.17, for Win64 (x86_64)
+--
+-- Host: localhost    Database: zimino
+-- ------------------------------------------------------
+-- Server version	8.0.17
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `menu`
+--
+
+DROP TABLE IF EXISTS `menu`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `menu` (
+  `menu_id` int(11) NOT NULL AUTO_INCREMENT,
+  `customer_id` int(11) DEFAULT NULL,
+  `description_drink` tinytext,
+  `description_appetizers` tinytext,
+  `description_main_course` tinytext,
+  `description_dessert` tinytext,
+  PRIMARY KEY (`menu_id`),
+  KEY `customer_id` (`customer_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `menu`
+--
+
+LOCK TABLES `menu` WRITE;
+/*!40000 ALTER TABLE `menu` DISABLE KEYS */;
+INSERT INTO `menu` VALUES (1,1,'Italian soda','Calamari Fritti','Spaghetti Al Salmone','Chocolate cake'),(2,2,'Coke','Carpaccio di salmone','Fettuccine ai Gamberoni','Cheesecake'),(3,3,'Diet Coke','Bruschetta','Linguine con Gamberetti','Tiramisu'),(4,4,'Sprite','Salad','Ravioli alla Aragosta',NULL),(5,5,'Fanta','Panzerotti','Linguine Di Granchio',NULL),(6,6,'Coffee',NULL,'Spiedini di polpettine',NULL),(7,7,'Wine',NULL,'Vegetariana',NULL),(8,8,NULL,NULL,'Fradiavola',NULL);
+/*!40000 ALTER TABLE `menu` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2019-10-07 18:16:18
---
+++ Zimino schema/zimino_view_insert_scrpit.sql
@@ -0,0 +1,21 @@
+use zimino;
+create view tips_view as 
+select tip 
+from ordered; 
+
+use zimino; 
+insert into menu
+(menu_id, customer_id, description_drink, 
+description_appetizers, description_main_course, description_dessert)
+VALUES  
+(1, 1, 'Italian soda', 'Calamari Fritti', 'Spaghetti Al Salmone', 'Chocolate cake'), 
+(2, 2, 'Coke', 'Carpaccio di salmone', 'Fettuccine ai Gamberoni', 'Cheesecake'),
+(3, 3, 'Diet Coke', 'Bruschetta', 'Linguine con Gamberetti', 'Tiramisu'),
+(4, 4, 'Sprite', 'Salad','Ravioli alla Aragosta', null), 
+(5, 5, 'Fanta', 'Panzerotti', 'Linguine Di Granchio', null),
+(6, 6, 'Coffee', null, 'Spiedini di polpettine', null),
+(7, 7, 'Wine', null, 'Vegetariana', null), 
+(8, 8, null, null, 'Fradiavola', null);
+
+
+
